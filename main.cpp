d095a139346e6dc4e373bb72bd33548fc80fb0df
wafonro <wagner.fonseca.rodrigues@gmail.com>
2019-10-26 19:39:59 -0300

    Introduce a simple SFML window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+
 int main() {
-  string name;
-  cout << "What's your name? ";
-  cin >> name;
-  cout << "Hello " << name << "!\n";
+  sf::RenderWindow window(sf::VideoMode(600, 600), "Mine game!");
+  sf::CircleShape circle(200.f, 30);
+  circle.setOrigin(200.f, 200.f);
+  circle.setPosition(300, 300);
+  circle.setFillColor(sf::Color::Green);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) window.close();
+    }
+
+    window.clear();
+    window.draw(circle);
+    window.display();
+  }
+
   return 0;
 }
